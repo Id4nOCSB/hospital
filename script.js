f8d6598a381a27e460948c49a04ba225e9fd3189
Id4nOCSB <idan.sheranosher@stu.ocsb.ca>
2025-04-06 01:52:41 +0000

    removed old code

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function showtime(){
-    document.getElementById("time").innerHTML = new Date().toUTCString();
-}
-showtime();
-setInterval(function(){
-    showtime();
-}
-, 1000);
 
 let hospitals = [];
 
